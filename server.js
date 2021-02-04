82030473727c9728f31d350e8ed1f5c320c5aec2
Domenic Beall <domenicbeall2@gmail.com>
2021-02-04 18:15:50 +1100

    Added routes for getting notes json

--- server.js
+++ server.js
@@ -10,10 +10,25 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// An array of notes
+const notes = [
+  {
+    title: "Test Note 1",
+    body: "Test note 1 body"
+  },
+  {
+    title: "Test Note 2",
+    body: "Test note 2 body"
+  }
+];
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/home.html'));
 })
 
+app.get('/api/getNotes', (req, res) => {
+  res.json(notes);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`)
