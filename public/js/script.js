55f8adf6e29f7d671f3f1fe04169bd289414bd53
DomenicBeall <domenicbeall2@gmail.com>
2021-02-09 18:09:02 +1100

    Added most of the functionality of the notes page

---
+++ public/js/script.js
@@ -0,0 +1,82 @@
+const saveBtn = document.getElementById('save-btn');
+const newNoteBtn = document.getElementById('new-note-btn');
+
+const noteTitle = document.getElementById('note-title');
+const noteBody = document.getElementById('note-body');
+const noteList = document.getElementById('note-list');
+
+
+loadNotes();
+
+function loadNotes() {
+    
+    fetch(`/api/notes`, {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+            const listNote = document.createElement(`div`);
+            listNote.setAttribute("class", "note");
+            listNote.setAttribute("data-noteid", data[i].id);
+            listNote.innerText = data[i].title;
+
+            listNote.addEventListener("click", () => {
+
+                fetch(`/api/note/${data[i].id}`, {
+                    method: 'GET',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    },
+                })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                });
+
+            });
+
+            noteList.append(listNote);
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+}
+
+
+newNoteBtn.addEventListener('click', () => {
+    noteTitle.value = "";
+    noteBody.value = "";
+
+    noteTitle.removeAttribute("disabled");
+    noteBody.removeAttribute("disabled");
+});
+
+
+saveBtn.addEventListener('click', () => {
+    let title = document
+        .getElementById('note-title')
+        .value.trim();
+    
+    let body = document
+        .getElementById('note-body')
+        .value.trim();
+
+    let note = { title: title, body: body};
+
+    fetch(`/api/notes`, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+});
