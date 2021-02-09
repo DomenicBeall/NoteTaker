23f369ac38d20ca8c1403fa51efb317c98a6885a
DomenicBeall <domenicbeall2@gmail.com>
2021-02-09 18:08:00 +1100

    Added nanoid to generate ids for notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const nanoid = require('nanoid');
+const { nanoid } = require('nanoid');
 const { json } = require('express');
 
 // Create constants
@@ -29,6 +29,18 @@ app.get('/api/notes', (req, res) => {
   res.json(db);
 });
 
+app.get('/api/note/:id', (req, res) => {
+  const id = req.params.id;
+  // Should read the db.json file and return all saved notes as JSON
+  const db = JSON.parse(fs.readFileSync('db.json', { encoding:'utf-8' }));
+
+  res.type('application/json');
+
+  res.json(db.find((element) => {
+    return element.id == id;
+  }));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
@@ -37,6 +49,7 @@ app.get('*', (req, res) => {
 app.post('/api/notes', (req, res) => {
   // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
   const note = req.body;
+  note.id = nanoid();
 
   const db = JSON.parse(fs.readFileSync('db.json', { encoding:'utf-8' }));
   
